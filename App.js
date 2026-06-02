afd146553a2e9e7acad14ac25d3a7c6e6e183e72
martubaumeister <mbaumeister@udesa.edu.ar>
2026-06-02 19:59:11 -0300

    Estructura base de carpetas, screens y componentes e instale la navegacion

--- App.js
+++ App.js
@@ -2,11 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Register from './src/screens/Register';
+import Login from './src/screens/Login';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/Register.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native-web";
+
+function Register(){
+  return (
+    <View>
+      <Text>Register</Text>
+    </View>
+  );
+}
+export default Register;
