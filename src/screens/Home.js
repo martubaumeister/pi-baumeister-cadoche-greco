992993d954bf77a3e7ff180e6fd3c4348fc4b79d
isabellagreco0 <igreco@udesa.edu.ar>
2026-06-03 17:42:24 -0300

    home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,59 @@
-import React from "react";
-import { View, Text } from "react-native-web";
-function Home(){
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  );
+import { View, FlatList, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { db } from '../firebase/Config';
+import Post from '../Componentes/Post';
+
+export default function Home({ navigation }) {
+
+    let [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+
+        db.collection('posts')
+            .orderBy('createdAt', 'desc')
+            .onSnapshot(docs => {
+
+                let postsArray = [];
+
+                docs.forEach(doc => {
+                    postsArray.push({
+                        id: doc.id,
+                        data: doc.data()
+                    })
+                })
+
+                setPosts(postsArray);
+
+            })
+
+    }, [])
+
+    return (
+        <View style={styles.container}>
+
+            <FlatList
+                data={posts}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => (
+
+                    <Post
+                        id={item.id}
+                        data={item.data}
+                       
+                    
+                    />
+
+                )}
+            />
+
+        </View>
+    )
 }
-export default Home;
+
+let styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#eaeaea'
+    }
+});
