b152efb2b8ede8982f46ed2650726608f413c3c8
martubaumeister <mbaumeister@udesa.edu.ar>
2026-06-06 11:22:14 -0300

    Hice el profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,11 +1,46 @@
 import React from "react";
-import { View, Text} from "react-native-web";
+import { View, Text, Pressable } from "react-native";
+import { StyleSheet } from "react-native";
+import { auth } from "../firebase/config";
 
-function Profile(){
+function Logout() {
+  auth.signOut()
+    .then(() => props.navigation.navigate('Login'))
+    .catch(error => console.log(error))
+}
+function Profile(props) {
   return (
-    <View>
-      <Text>Perfil</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Mi perfil</Text>
+      <Text style={styles.text}>Email: {auth.currentUser.email}</Text>
+
+      <Pressable style={styles.boton} onPress={Logout}>
+        <Text style={styles.textoBoton}>Cerrar sesión.</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+
+  },
+  boton: {
+    backgroundColor: "rgb(75, 75, 234)",
+    borderRadius: 10, 
+    padding: 10, 
+    marginTop: 15, 
+    alignItems: "center",
+    width: 200, 
+    alignSelf:'center',
+  },
+  text:{
+    fontSize: 16, 
+    marginBottom: 10,
+  },
+  textoBoton: {
+    color: 'white',
+    fontSize: 16,
+  }
+})
 export default Profile;
--- src/screens/Register.js
+++ src/screens/Register.js
@@ -9,13 +9,13 @@ function Register(props) {
   const [password, setPassword] = useState('');
   const [registerError, setRegisterError] = useState('');
 
-  useEffect(()=> {
-    auth.onAuthStateChanged( user => {
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
       console.log(user)
       if (user != null) {
         props.navigation.navigate('HomeMenu');
       }
-    })   
+    })
   }, [])
 
   function onSubmit() {
@@ -34,7 +34,7 @@ function Register(props) {
       })
   }
 
-  function redireccionar(){
+  function redireccionar() {
     props.navigation.navigate('Login');
   }
 
@@ -61,7 +61,7 @@ function Register(props) {
         <Text> Registrate </Text>
       </Pressable>
 
-      {registerError == "" ? "" : <Text style={styles.error} >{registerError}</Text>} 
+      {registerError == "" ? "" : <Text style={styles.error} >{registerError}</Text>}
 
       <Pressable style={styles.button} onPress={() => redireccionar()}>
         <Text> Ya tenes cuenta? Inicia sesión </Text>
@@ -75,7 +75,7 @@ export default Register;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#ffffff", 
+    backgroundColor: "#ffffff",
     justifyContent: "center",
     alignItems: "center",
     paddingHorizontal: 30
@@ -87,7 +87,7 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     marginBottom: 12,
     fontSize: 12,
-    backgroundColor:"rgb(165, 165, 165)",
+    backgroundColor: "rgb(165, 165, 165)",
   },
   button: {
     backgroundColor: "rgb(75, 75, 234)",
@@ -97,7 +97,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginBottom: 12,
     borderWidth: 1,
-  }, 
+  },
   error: {
     color: "#ff0000",
     textAlign: "center",
